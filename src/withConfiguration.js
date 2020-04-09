c82d425aa7136f108b4e3af6404f89a391ddaeea
mrroll <58090032+mrroll@users.noreply.github.com>
2020-04-10 02:10:05 +0800

    - Remove asset prefix - Update dependency.

--- src/withConfiguration.js
+++ src/withConfiguration.js
@@ -5,30 +5,25 @@ const cssModules = require("./cssModules");
 const minifyCss = require("./minifyCss");
 
 module.exports = function withConfiguration(nextConfig = {}) {
-  const assetPrefix = process.env.EXPORT_RELATIVE ? "." : "";
   const defaultOptions = {
-    // For filesystem based routing if needed.
-    assetPrefix,
-
     /**
      * Mimic CRA's ability to pass on specific env variables.
      * https://create-react-app.dev/docs/adding-custom-environment-variables/
      */
     env: getEnvironment(),
 
-    /**
-     *
-     * Plugins
-     *
-     */
-
+    // Plugins
     // next-optimized-images
     optimizeImages: true,
   };
   return Object.assign(
+    // Empty Object
     {},
-    // Our default options unless config options are overridden in next.config.js
-    { ...defaultOptions, ...nextConfig },
+    // Our default options
+    { ...defaultOptions },
+    // unless config options are overridden in next.config.js
+    { ...nextConfig },
+    // Finally, webpack.
     {
       webpack: function getWebpackConfig(config, options) {
         const { buildId, dev, isServer, defaultLoaders, webpack } = options;
@@ -48,6 +43,14 @@ module.exports = function withConfiguration(nextConfig = {}) {
         // Minify stuff
         // https://spectrum.chat/next-js/general/minification-of-css~8d5458ee-39e3-4706-a666-6c471e9fc7f8?m=MTU1MDY1MjIwNzU1NA==
         minifyCss({ config, dev });
+
+        /**
+         * the webpack key is overridden with this so let's try to get the
+         * passed in config as well
+         */
+        if (typeof nextConfig.webpack === "function") {
+          return nextConfig.webpack(config, options);
+        }
 
         return config;
       },
