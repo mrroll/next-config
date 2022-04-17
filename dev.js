6c8fb72242591237630345407eb92e518a031c51
mrroll <58090032+mrroll@users.noreply.github.com>
2022-04-17 18:52:52 +0800

    12.0.3

--- dev.js
+++
@@ -1,9 +0,0 @@
-const { spawn } = require("child_process");
-const chokidar = require("chokidar");
-
-// One-liner for current directory
-chokidar.watch("./src").on("all", (event, path) => {
-  if (["add", "change", "unlink"].includes(event)) {
-    spawn("yalc", ["push"], { stdio: "inherit" });
-  }
-});
